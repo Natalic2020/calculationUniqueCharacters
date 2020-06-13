bc69971c7d8891e8579f5b8284b7ce49899d2ee1
Nata <Nata@mail.ru>
2020-06-13 13:41:20 +0200

    remove comment

--- src/main/java/ua/com/foxminded/calculation/Calculation.java
+++ src/main/java/ua/com/foxminded/calculation/Calculation.java
@@ -1,19 +1,13 @@
 package ua.com.foxminded.calculation;
 
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.StringJoiner;
-import java.util.stream.Collectors;
 
 public class Calculation {
 
 	public String calculationUniqueCharacters(final String text) {
-		
+
 		if (text == null) {
 			throw new IllegalArgumentException("Null parameters are not allowed");
 		}
@@ -34,17 +28,7 @@ public class Calculation {
 			outputStringJoiner.add(String.format("\"%s\" - %d%n", symbol, (int) count));
 			uniqueCharactersHashMap.put(symbol, (int) count);
 		}
-
 		cacheHashMap.put(text, outputStringJoiner.toString());
-		// System.out.print(String.format("%s%n",text) + myStringJoiner.toString());
-//		System.out.println(myStringJoiner.toString());
-//		for ( Map.Entry<Character, Integer> entry : myHashMap.entrySet()) {
-//			Character key = entry.getKey();
-//			Integer value = entry.getValue();
-//		    // ...
-//		}
-
 		return String.format("%s%n", text) + outputStringJoiner.toString();
 	}
-
 }
